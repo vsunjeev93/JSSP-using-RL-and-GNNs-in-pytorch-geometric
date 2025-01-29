63496f65504d595287c6f35d9018c45182685d9b
Sunjeev Venkateswaran <vsunjeev93@gmail.com>
2025-01-28 20:09:04 -0600

    Update train_JSSP.py

--- train_JSSP.py
+++ train_JSSP.py
@@ -36,8 +36,6 @@ actor = actor(input_features, 512)
 actor.to(device)
 critic = critic(input_features, 512)
 critic.to(device)
-actor.train()
-critic.train()
 torch.manual_seed(args.seed)
 # setting optimizer, learning rate and other training parameters
 LR = 0.0001
